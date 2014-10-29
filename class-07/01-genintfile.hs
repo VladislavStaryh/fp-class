5d019aa74c994d643ee17e17e3ff60bafce8233e
student <student@r1-01.(none)>
2014-10-29 10:16:50 +0400

    1-st done

--- class-07/01-genintfile.hs
+++ class-07/01-genintfile.hs
@@ -7,4 +7,33 @@
   4) количество строк в файле.
 -}
 
-main = undefined
+import System.Environment
+import System.Random
+
+createRandomStr :: Int -> Int -> Int -> StdGen -> (String, StdGen)
+createRandomStr _ _ 0 gen = ("",gen)
+createRandomStr from to 1 gen = let (x, newGen) = randomR(from,to) gen :: (Int, StdGen)
+					in ((show x), newGen)
+
+createRandomStr from to n_num gen = let (x, newGen) = randomR(from,to) gen :: (Int, StdGen) 
+				    	(tail, newGen') = (createRandomStr from to (n_num-1) newGen)
+						in ((((show x)++", ")++tail), newGen')
+
+
+createRandomText :: Int -> Int -> Int -> Int -> StdGen -> [String]
+createRandomText _ _ _ 0 gen = []
+createRandomText from to n_num 1 gen = let (str, newGen) = createRandomStr from to n_num gen
+	in [str]
+createRandomText from to n_num n_str gen = let (str, newGen) = createRandomStr from to n_num gen
+	in str:(createRandomText from to n_num (n_str-1) newGen)
+
+
+createRandomFile :: String -> Int -> Int -> Int -> Int -> IO ()
+createRandomFile fname from to n_num n_str = do
+	gen <- newStdGen
+	let int_lines = createRandomText from to n_num n_str gen
+	writeFile fname (unlines int_lines)
+
+main = do
+  [fname, from, to, n_num, n_str] <- getArgs
+  createRandomFile fname (read from) (read to) (read n_num) (read n_str)

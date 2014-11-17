9c45fd58f5dbf7b65206d6b1e11299db648ffac4
vvs <vlad.star.08@gmail.com>
2014-11-18 01:14:45 +0300

    10.2 done

--- class-10/02-rope-walker.hs
+++ class-10/02-rope-walker.hs
@@ -29,7 +29,7 @@ updatePole :: Pole -> Either String Pole
 updatePole p = if unbalancedRight p then Left "Right side unbalanced" else if unbalancedLeft p then Left "Left side unbalanced" else Right p
   where
 	unbalancedRight (l, r) =  r-l > balance
-	unbalancedRight (l, r) =  l-r > balance
+	unbalancedLeft (l, r) =  l-r > balance
 
 
 landLeft :: Birds -> Pole -> Either String Pole
@@ -38,15 +38,25 @@ landLeft n (left, right) = updatePole (left + n, right)
 landRight :: Birds -> Pole -> Either String Pole
 landRight n (left, right) = updatePole (left, right + n)
 
+landBalanced :: Birds -> Pole -> Either String Pole
+landBalanced n (left, right) = updatePole (left+n, right + n)
+
 banana :: Pole -> Either String Pole
 banana = const (Left "Banana is reason")
 
 unlandAll :: Pole -> Either String Pole
 unlandAll = const (Right (0,0))
 
-tests = all test [1..3]
-  where
-    test 1 = (return (0, 0) >>= landLeft 1 >>= landRight 4 
-              >>= landLeft (-1) >>= landRight (-2)) == Nothing
-    test 2 = (return (0, 0) >>= landRight 2 >>= landLeft 2 >>= landRight 2) == Just (2, 4)
-    test 3 = (return (0, 0) >>= landLeft 1 >>= banana >>= landRight 1) == Nothing
+actionByString :: Pole -> String -> Either String Pole
+actionByString  p "B" = banana p
+actionByString  p "UA" = unlandAll p
+actionByString p str = let (s , n) = (head str, read $ drop 2 str :: Birds) in
+		if s == 'R' then landRight n p else
+		if s == 'B' then landBalanced n p else landLeft n p
+
+--tests = all test [1..3]
+  --where
+    --test 1 = (return (0, 0) >>= landLeft 1 >>= landRight 4 
+      --        >>= landLeft (-1) >>= landRight (-2)) == Nothing
+    --test 2 = (return (0, 0) >>= landRight 2 >>= landLeft 2 >>= landRight 2) == Just (2, 4)
+    --test 3 = (return (0, 0) >>= landLeft 1 >>= banana >>= landRight 1) == Nothing
